67515d21aea33f8da75e61df7ce72f687656de46
EAM_VM <vasanth@eliteappmakers.com>
2024-04-25 16:25:45 +0530

    Feat: Changed the comments

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ const data: MyData = {
 };
 
 export const generateRandomNumber = (min: number, max: number): number => {
-    // Ensure min is less than max, swap values if needed
+    // Ensure min is less than max, swap values if needed //
     if (min > max) {
       [min, max] = [max, min];
     }
   
-    // Generate a random number within the specified range
+    // Generate a random number within the specified range //
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     return randomNumber;
   }
